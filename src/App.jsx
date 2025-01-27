d4849c00f6c05c535fe37e16c2029eaf6ed8b043
Plasma <pandeyup111@gmail.com>
2025-01-27 15:04:29 +0545

    Making a  movies finder app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="" alt="Hero Image Background" />
+            <h1>Find <span className='text-gradient'>Movies</span> You ll Enjoy Without the Hassle</h1>
+          </header>
+
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
